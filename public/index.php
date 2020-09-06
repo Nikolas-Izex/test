cd0a63499d57c8dc49f0c619657d4668dd20fe17
Nikolay Su <nikolay@izex.org>
2020-09-06 11:48:45 +0300

    commit n3

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!--https://github.com/Nikolas-Izex/test
+RUN GIT AGAIN-->
+<?php
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+require '../vendor/autoload.php';
+
+
+echo 'Hello new feautures';
+
+// import the Intervention Image Manager Class
+//use Intervention\Image\ImageManager;
+//
+//// create an image manager instance with favored driver
+//$manager = new ImageManager(array('driver' => 'imagick'));
+//
+//// to finally create image instances
+//$image = $manager->make('foo.jpg')->resize(300, 20)->save('bar.jpg');
+//$mime = $manager->make('foo.jpg')->mime();
+//echo $mime;
